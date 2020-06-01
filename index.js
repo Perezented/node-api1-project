3e79b2cf723958d85a1a997d8039fde549b61d19
Michael Perez <mperez6473@gmail.com>
2020-06-01 14:19:42 -0500

    Added the get and post for users. The warning are not built in yet.

---
+++ index.js
@@ -0,0 +1,39 @@
+//  Import express
+const express = require('express');
+
+//  creating the server
+const server = express();
+
+//  middleware
+server.use(express.json()); // parsing JSON from  the body
+
+//  function to handle GET '/'
+server.get('/', (res, req) => {
+    res.statusCode(200).json(`Hey there, welcome to port ${port}`);
+});
+
+//  list of users
+let users = [
+    {
+        id: 'a_unique_id', // hint: use the shortid npm package to generate it
+        name: 'Jane Doe', // String, required
+        bio: "Not Tarzan's Wife, another Jane", // String, required
+    },
+];
+
+//  function to handle GET '/api/users'
+server.get('/api/users', (res, req) => {
+    res.status(200).json(users);
+});
+//function to handle POST '/api/users'
+server.post('/api/users', (res, req) => {
+    const user = req.body;
+    users.push(user);
+    res.status(201).json(users);
+});
+
+//  setting up the port
+const port = 9000;
+server.listen(port, () => {
+    console.log(`server port ${port} is listening`);
+});
